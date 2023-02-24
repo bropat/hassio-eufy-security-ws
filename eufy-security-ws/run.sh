bccc85d9dee7133eb7fae84309429a6eb4fe0dcf
bropat <patrick.broetto@gmail.com>
2023-02-24 18:08:25 +0000

    Fixed run.sh config generation issue

--- eufy-security-ws/run.sh
+++ eufy-security-ws/run.sh
@@ -4,53 +4,34 @@ CONFIG_PATH=/data/eufy-security-ws-config.json
 
 USERNAME="$(bashio::config 'username')"
 PASSWORD="$(bashio::config 'password')"
+COUNTRY="$(bashio::config 'country')"
+EVENT_DURATION_SECONDS="$(bashio::config 'event_duration')"
+POLLING_INTERVAL_MINUTES="$(bashio::config 'polling_interval')"
+ACCEPT_INVITATIONS="$(bashio::config 'accept_invitations')"
+TRUSTED_DEVICE_NAME="$(bashio::config 'trusted_device_name')"
 
-COUNTRY_ARG=""
 COUNTRY_JQ=""
 if bashio::config.has_value 'country'; then
-    COUNTRY_ARG="--arg country $(bashio::config 'country')"
     COUNTRY_JQ="country: \$country,"
 fi
 
-EVENT_DURATION_SECONDS_ARG=""
 EVENT_DURATION_SECONDS_JQ=""
 if bashio::config.has_value 'event_duration'; then
-    EVENT_DURATION_SECONDS_ARG="--arg event_duration_seconds $(bashio::config 'event_duration' '')"
-    EVENT_DURATION_SECONDS_JQ="eventDurationSeconds: \$event_duration_seconds,"
+    EVENT_DURATION_SECONDS_JQ="eventDurationSeconds: \$event_duration_seconds|tonumber,"
 fi
 
-LANGUAGE_ARG=""
-LANGUAGE_JQ=""
-#if bashio::config.has_value 'language'; then
-#    LANGUAGE_ARG="--arg language $(bashio::config 'language')"
-#    LANGUAGE_JQ="language: \$language,"
-#fi
-
-P2P_CONNECTION_SETUP_ARG=""
-P2P_CONNECTION_SETUP_JQ=""
-#if [ -n "${P2P_CONNECTION_SETUP}" ]; then
-#    P2P_CONNECTION_SETUP_ARG="--arg p2p_connection_setup $P2P_CONNECTION_SETUP"
-#    P2P_CONNECTION_SETUP_JQ="p2pConnectionSetup: \$p2p_connection_setup,"
-#fi
-
-POLLING_INTERVAL_MINUTES_ARG=""
 POLLING_INTERVAL_MINUTES_JQ=""
 if bashio::config.has_value 'polling_interval'; then
-    POLLING_INTERVAL_MINUTES_ARG="--arg polling_interval_minutes $(bashio::config 'polling_interval')"
-    POLLING_INTERVAL_MINUTES_JQ="pollingIntervalMinutes: \$polling_interval_minutes,"
+    POLLING_INTERVAL_MINUTES_JQ="pollingIntervalMinutes: \$polling_interval_minutes|tonumber,"
 fi
 
-ACCEPT_INVITATIONS_ARG=""
 ACCEPT_INVITATIONS_JQ=""
 if bashio::config.true 'accept_invitations'; then
-    ACCEPT_INVITATIONS_ARG="--arg accept_invitations $(bashio::config 'accept_invitations')"
     ACCEPT_INVITATIONS_JQ="acceptInvitations: \$accept_invitations,"
 fi
 
-TRUSTED_DEVICE_NAME_ARG=""
 TRUSTED_DEVICE_NAME_JQ=""
 if bashio::config.has_value 'trusted_device_name'; then
-    TRUSTED_DEVICE_NAME_ARG="--arg trusted_device_name $(bashio::config 'trusted_device_name')"
     TRUSTED_DEVICE_NAME_JQ="trustedDeviceName: \$trusted_device_name,"
 fi
 
@@ -77,7 +58,7 @@ fi
 
 PORT_OPTION=""
 if bashio::config.has_value 'port'; then
-    PORT_OPTION="--port $(bashio::config 'port' '3000')"
+    PORT_OPTION="--port $(bashio::config 'port')"
 fi
 
 DEBUG_OPTION=""
@@ -88,13 +69,11 @@ fi
 JSON_STRING="$( jq -n \
   --arg username "$USERNAME" \
   --arg password "$PASSWORD" \
-  $COUNTRY_ARG \
-  $EVENT_DURATION_SECONDS_ARG \
-  $LANGUAGE_ARG \
-  $P2P_CONNECTION_SETUP_ARG \
-  $POLLING_INTERVAL_MINUTES_ARG \
-  $TRUSTED_DEVICE_NAME_ARG \
-  $ACCEPT_INVITATIONS_ARG \
+  --arg country "$COUNTRY" \
+  --arg event_duration_seconds "$EVENT_DURATION_SECONDS" \
+  --arg polling_interval_minutes "$POLLING_INTERVAL_MINUTES" \
+  --arg trusted_device_name "$TRUSTED_DEVICE_NAME" \
+  --arg accept_invitations "$ACCEPT_INVITATIONS" \
   $STATION_IP_ADDRESSES_ARG \
     "{
       username: \$username,
@@ -102,8 +81,6 @@ JSON_STRING="$( jq -n \
       persistentDir: \"/data\",
       $COUNTRY_JQ
       $EVENT_DURATION_SECONDS_JQ
-      $LANGUAGE_JQ
-      $P2P_CONNECTION_SETUP_JQ
       $POLLING_INTERVAL_MINUTES_JQ
       $TRUSTED_DEVICE_NAME_JQ
       $ACCEPT_INVITATIONS_JQ
